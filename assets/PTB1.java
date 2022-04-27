ace3b23f88374a8098d959e46ad52658b9b5fdb6
Binz17 <104473658+Binz17@users.noreply.github.com>
2022-04-27 10:42:07 +0700

    Add files via upload
    post file

---
+++ assets/PTB1.java
@@ -0,0 +1,18 @@
+
+public class PTB1 {
+	
+	double hsa, hsb;
+	void nhap( ) {
+		System.out.println("nhap cac he so cho phuong trinh bac 1");
+	}
+	void giai( ) {
+		System.out.println("\n Giai phuong trinh bac 1");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PTB1 m= new PTB1 ();
+		m.nhap();
+		m.giai();
+	}
+
+}
